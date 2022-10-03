dfca6507dfd7373c81f683e0926ca1e0a4bb2dcf
Yilin Wang <Doom@wang2681799845.wifi.local.cmu.edu>
2022-10-03 14:43:36 -0400

    fixed bugs

--- EDA.py
+++ EDA.py
@@ -45,7 +45,7 @@ df = pd.DataFrame(
 
 def plot_tsne(color_by = "consumption"):
     my_plot = sns.scatterplot("x_0", "x_1", hue = color_by, data= df)
-    plt.save(f"figures/tsne_plot_by_{color_by}")
+    plt.savefig(f"figures/tsne_plot_by_{color_by}")
 
 if __name__ == "__main__":
     plot_tsne(color_by = "consumption")
