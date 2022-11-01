222d3fe0287de54985ea17b1a878e0bf60bc3ffc
Yilin Wang <yilinwan@andrew.cmu.edu>
2022-11-01 15:13:46 -0400

    fixed bugs

--- train.py
+++ train.py
@@ -54,6 +54,8 @@ def train(model, optimizer, train_dataloader, device, EPOCHS, ckpt_dir = None, l
                           token_type_ids=None
                         )
       loss = outputs[0]  
+      print(output)
+      print(loss)
       batch_loss = loss.item()
       total_train_loss += batch_loss
 
